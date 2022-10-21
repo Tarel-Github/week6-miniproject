d09d6b16cb3ab700908d625bcde34a31918ec3f2
[Github <[Github>
2022-10-21 17:59:03 +0900

    브렌치 업로드

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const a = 1;
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const jwt = require("jsonwebtoken")      
+const app = express();
+const router = express.Router();
+const SECRET_KEY = `customized-secret-key`;
+const routes = require('./routes');
 
-console.log (a);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended:false}));
+
+app.use('/api', routes);
+
+app.listen(8080, () => {
+    console.log("서버 가동");
+  });
+
