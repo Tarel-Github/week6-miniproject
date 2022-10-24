bee31e4fec795c2cc860c83e515875024cb49cf2
ninthsun91 <50778955+ninthsun91@users.noreply.github.com>
2022-10-24 21:10:03 +0900

    Update index.js

--- models/index.js
+++ models/index.js
@@ -5,22 +5,9 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-<<<<<<< Updated upstream
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
-
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-=======
 const db = {};
 
 const sequelize = require('../config/connection');
->>>>>>> Stashed changes
 
 fs
   .readdirSync(__dirname)
