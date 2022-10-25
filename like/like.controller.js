33bace5ea27db0ce9cf15118419fc6d890cfcb7d
[Github <[Github>
2022-10-25 23:18:28 +0900

    미들웨어 적용

--- like/like.controller.js
+++ like/like.controller.js
@@ -6,8 +6,8 @@ class LikesController{
     getPostLike = async (req, res, next) => {
         try {
             //#########################
-            const userId = 1 //로그인 기능 구현시 수정 필요
-            //const { userId } = res.locals.user;                             //로그인한 유저의 userId를 가져오고
+            //const userId = 1 //로그인 기능 구현시 수정 필요
+            const { userId } = res.locals.user;                             //로그인한 유저의 userId를 가져오고
             //#########################  
 
             const Likes = await this.likeService.getPostLike(userId, );     //그 데이터를 서비스에 보내고 받아온 정보를
@@ -23,8 +23,8 @@ class LikesController{
             const { postId } = req.params;                      //postId를 파람스로부터 가져옴
 
             //#########################
-            const userId = 2 //로그인 기능 구현시 수정 필요
-            //const { userId } = res.locals.user;                             //로그인한 유저의 userId를 가져오고
+            //const userId = 2 //로그인 기능 구현시 수정 필요
+            const { userId } = res.locals.user;                             //로그인한 유저의 userId를 가져오고
             //#########################     
             
             const Likes = await this.likeService.updatePostLike(userId, postId);    //위의 두개를 서비스로 보내고 받아온 값을
--- like/like.route.js
+++ like/like.route.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
-//const authMiddleware = require('../middlewares/authMiddlewars')
+const auth = require('../middlewares/authMiddleware')
 
 const LikesController = require('./like.controller');
 const likesController = new LikesController();
 
 //좋아요 게시글 조회
-router.get('/', /*authMiddleware,*/likesController.getPostLike)
+router.get('/', auth, likesController.getPostLike)
 //좋아요 등록 혹은 취소
-router.put('/:postId', /*authMiddleware,*/ likesController.updatePostLike)
+router.put('/:postId', auth, likesController.updatePostLike)
 
 
 
